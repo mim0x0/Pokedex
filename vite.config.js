8fa4e0f855110bb9257fa556347c533bb32e3ed9
mim0x0 <mimbarilham1@gmail.com>
2025-09-03 14:14:57 +0800

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: 'https://mim0x0.github.io/Pokedex/',
+  base: '/Pokedex/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
